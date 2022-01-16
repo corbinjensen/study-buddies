060285300a0294ecf16e6ffbab4a711378e74e62
Richard Fu <3363508-richardfuca@users.noreply.gitlab.com>
2022-01-16 02:14:02 -0800

    Add Azure UI

--- my-app/src/AzureCaller.jsx
+++ my-app/src/AzureCaller.jsx
@@ -3,7 +3,19 @@ import { AzureCommunicationTokenCredential } from '@azure/communication-common';
 import React, {useEffect, useRef} from 'react';
 import {Button, Form} from "react-bootstrap";
 import DataStore from './dataStore';
+import {
+  CameraButton,
+  ControlBar,
+  EndCallButton,
+  GridLayout,
+  MicrophoneButton,
+  DevicesButton,
+  ScreenShareButton,
+  VideoGallery
+} from '@azure/communication-react';
 
+import { FluentThemeProvider, DEFAULT_COMPONENT_ICONS } from '@azure/communication-react';
+import { registerIcons } from '@fluentui/react';
 export default function AzureCaller() {
   const [callClient, setCallClient] = React.useState();
   const [callAgent, setCallAgent] = React.useState();
@@ -13,8 +25,9 @@ export default function AzureCaller() {
   const [token, setToken] = React.useState('');
   const [number, setNumber] = React.useState('');
   const [identity, setIdentity] = React.useState({});
-  const localVideoRef = useRef();
-  const buddyVideoRef = useRef();
+  const [localVideoStream, setLocalVideoStream] = React.useState();
+  const [remoteVideoStream, setRemoteVideoStream] = React.useState();
+  registerIcons({ icons: DEFAULT_COMPONENT_ICONS });
 
   useEffect(() => {
     async function init() {
@@ -28,7 +41,7 @@ export default function AzureCaller() {
       }, ).then(res => res.json());
       setUserId(identityToken.user.communicationUserId);
 
-      const callClient = new CallClient();
+      const callClient = new CallClient()
       console.log(token);
       const tokenCredential = new AzureCommunicationTokenCredential(token || identityToken.token);
       const callAgent = await callClient.createCallAgent(tokenCredential, { displayName: 'test' });
@@ -40,6 +53,15 @@ export default function AzureCaller() {
       setCallClient(callClient);
       setDeviceManager(deviceManager);
       setCallAgent(callAgent);
+
+      const camera = (await deviceManager.getCameras())[0];
+      const stream = camera ? new LocalVideoStream(camera) : undefined;
+
+      if (stream) {
+        const renderer = new VideoStreamRenderer(stream);
+        const view = await renderer.createView();
+        setLocalVideoStream(view.target);
+      }
     }
     init();
   }, [token]);
@@ -58,7 +80,7 @@ export default function AzureCaller() {
     if (stream) {
       const renderer = new VideoStreamRenderer(stream);
       const view = await renderer.createView();
-      localVideoRef.current.appendChild(view.target);
+      setLocalVideoStream(view.target);
     }
     const videoOptions = stream ? { localVideoStreams: [stream] } : undefined;
 
@@ -72,13 +94,13 @@ export default function AzureCaller() {
       console.log(e);
       onIncomingCall(e);
     };
-    if (deviceManager && buddyVideoRef.current && callAgent) {
+    if (deviceManager && callAgent) {
       callAgent.on('incomingCall', callbackFn);
       return () => {
         callAgent.off('incomingCall', callbackFn);
       };
     }
-  }, [callAgent, deviceManager, buddyVideoRef]);
+  }, [callAgent, deviceManager]);
 
   async function makeCall(num) {
     const camera = (await deviceManager.getCameras())[0];
@@ -87,7 +109,7 @@ export default function AzureCaller() {
     if (stream) {
       const renderer = new VideoStreamRenderer(stream);
       const view = await renderer.createView();
-      localVideoRef.current.appendChild(view.target);
+      setLocalVideoStream(view.target);
     }
     const videoOptions = stream ? { localVideoStreams: [stream] } : undefined;
 
@@ -130,39 +152,50 @@ export default function AzureCaller() {
   async function startRemoteVideo(stream) {
     const renderer = new VideoStreamRenderer(stream);
     const view = await renderer.createView();
-    buddyVideoRef.current.appendChild(view.target);
+    setRemoteVideoStream(view.target);
   }
 
   function hangUp() {
     currentCall.hangUp({ forEveryone: true });
     setCurrentCall(null);
-    localVideoRef.current.innerHTML = '';
-    buddyVideoRef.current.innerHTML = '';
+    setRemoteVideoStream(null);
   }
-console.log(identity);
-  return <>
+  return <FluentThemeProvider style={{ height: '70%' }}>
     {callAgent == null && <p>Loading...</p>}
     {callAgent != null && <p>You are now ready to find your Study Buddy™!</p>}
-    {callAgent != null && typeof identity === 'object' && Object.keys(identity).length > 0 && <Form.Select onChange={(event) => {
-      const e = event.target.value;
-      setNumber(e == 'null' ? '' : e);
-    }}>
-      <option value={"null"}>Please select one...</option>
-      {Object.keys(identity).filter(e => e !== DataStore.getName()).map(e => <option value={identity[e].user.communicationUserId} >{e}</option>)}
-    </Form.Select>}
-    <Button
-    disabled={currentCall != null || callAgent == null || number.length === 0}
-    onClick={makeCall}>
-      Call <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
-</svg>
-      </Button>
-    <Button disabled={currentCall == null} onClick={hangUp}>Hangup <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-x-fill" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zm9.261 1.135a.5.5 0 0 1 .708 0L13 2.793l1.146-1.147a.5.5 0 0 1 .708.708L13.707 3.5l1.147 1.146a.5.5 0 0 1-.708.708L13 4.207l-1.146 1.147a.5.5 0 0 1-.708-.708L12.293 3.5l-1.147-1.146a.5.5 0 0 1 0-.708z"/>
-</svg></Button>
-    <p>Your Video:</p>
-    <div ref={localVideoRef} style={{ height: '100px', width: '180px' }}/>
-    <p>Study Buddy Video:</p>
-    <div ref={buddyVideoRef} style={{ height: '200px', width: '360px' }}/>
-  </>;
+
+    { callAgent != null && <div style={{ height: '75px' }}>
+      <ControlBar styles={{ root: { height: '75px', justifyContent: 'center'} }}>
+        {callAgent != null && typeof identity === 'object' && Object.keys(identity).length > 0 && <Form.Select onChange={(event) => {
+          const e = event.target.value;
+          setNumber(e == 'null' ? '' : e);
+        }} style={{ width: '40%' }}>
+          <option value={"null"}>Please select one...</option>
+          {Object.keys(identity).filter(e => e !== DataStore.getName()).map(e => <option value={identity[e].user.communicationUserId} >{e}</option>)}
+        </Form.Select>}
+        <EndCallButton
+          disabled={number == 'null'}
+          styles={{
+            root: currentCall == null ? { background: '#7FBA00' } : {},
+            rootHovered: currentCall == null ? { background: '#7FBA00' } : {},
+            rootChecked: currentCall == null ? { background: '#7FBA00' } : {},
+            rootPressed: currentCall == null ? { background: '#7FBA00' } : {},
+        }}
+          onHangUp={currentCall == null ? makeCall : hangUp}
+          strings={{ tooltipContent: currentCall == null ? 'Make Call' : 'Leave Call'}}
+        />
+        <CameraButton />
+        <MicrophoneButton />
+        <ScreenShareButton
+        />
+        <DevicesButton />
+      </ControlBar>
+    </div>}
+
+    {callAgent != null && <VideoGallery
+      localParticipant={{ userId: '1', displayName: 'You!', videoStream: { renderElement: localVideoStream } }}
+      remoteParticipants={remoteVideoStream == null ? [] : [{ userId: '2', displayName: 'Study Buddy', videoStream: { isAvailable: true, renderElement: remoteVideoStream } }]}
+      maxRemoteVideoStreams={99}
+    />}
+  </FluentThemeProvider>;
 }

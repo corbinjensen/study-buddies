bb3bdf3d7e5aa5ab821b51b21c31bee76f6b8ffa
Corbin Jensen <corbinjensen@users.noreply.github.com>
2022-01-15 21:32:48 -0800

    moved Azure caller to chat page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import {Outlet} from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
-import AzureCaller from './AzureCaller';
 
 function App() {
   return (
     <div className="p-3">
       <Header />
       <Outlet />
-      <AzureCaller/>
       <Footer />
     </div>
   );
--- my-app/src/pages/Chat.js
+++ my-app/src/pages/Chat.js
@@ -1,7 +1,11 @@
+import React from 'react';
+import AzureCaller from '../AzureCaller';
+
 export default function Chat() {
     return (
-      <main style={{ padding: "1rem 0" }}>
+      <>
         <h2>Chat</h2>
-      </main>
+        <AzureCaller/>
+      </>
     );
   }

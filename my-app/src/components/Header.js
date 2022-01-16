c475f629f6998a9d17de4306b38969d84b5cc494
Corbin Jensen <corbinjensen@users.noreply.github.com>
2022-01-16 10:21:23 -0800

    changed favicon

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -4,7 +4,7 @@ import NavComp from './NavComp'
 const Header = () => (
 
             <header className="container-fluid">
-                <h1>Study Buddy App</h1>
+                <h1>Study Buddy</h1>
                 <NavComp />
                 <hr></hr>
             </header>
